b193c6496520afb2328eec5afa57215c53d3af59
Suryaprakash <suryaprakash.kumar@outlook.com>
2026-03-19 22:24:44 +0530

    updated the env data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,13 @@ export const environment = {
     // TODO: Create authorized domains in Auth → Settings → Authorized domains
     // TODO: Create Firestore database: Firestore Database → Create database
     // TODO: Set Firestore rules to allow authenticated users
-    apiKey: 'AIzaSyAKZfnA6-t9wMLB1LGgh9FrFmABApDX3Sw', // This is specific to your registered app and needs to be retrieved from the Firebase console.
-    authDomain: 'the-haircraft.firebaseapp.com',
-    projectId: 'the-haircraft',
-    storageBucket: 'the-haircraft.firebasestorage.app',
-    messagingSenderId: '360134247068',
-    appId: '1:360134247068:web:c30ff714c88b2a5898e359', // This is specific to your registered app and needs to be retrieved from the Firebase console.
-    measurementId: 'G-XXXXXXXXXX' // This is present if Google Analytics is enabled for your app.
+    apiKey: '', // This is specific to your registered app and needs to be retrieved from the Firebase console.
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '', // This is specific to your registered app and needs to be retrieved from the Firebase console.
+    measurementId: '' // This is present if Google Analytics is enabled for your app.
   },
-  razorpayKey: 'rzp_test_thehaircraft'
+  razorpayKey: ''
 };
