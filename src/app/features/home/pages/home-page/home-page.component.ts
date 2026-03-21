8f4495eaafd91eb09155e9aded14eb8758e4d26b
Suryaprakash <suryaprakash.kumar@outlook.com>
2026-03-21 14:06:30 +0530

    video added on home page

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -38,6 +38,8 @@ export class HomePageComponent {
       price: 'From $189',
       type: '100% Remy',
       image: 'assets/images/raw_indian_hair.jpg',
+      video:
+        'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FRaw%20Indian%20single%20donor%20hair.MOV?alt=media&token=0bc77368-d7be-4889-a4ca-70ae081f5642',
       videos: [
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FRaw%20Indian%20single%20donor%20hair.MOV?alt=media&token=0bc77368-d7be-4889-a4ca-70ae081f5642',
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FRaw%20Indian%20single%20donor%20hair2.MOV?alt=media&token=86a7af18-5763-4813-b8eb-8d9bb88ee3ae'
@@ -50,6 +52,8 @@ export class HomePageComponent {
       price: 'From $119',
       type: 'Swiss Lace',
       image: 'assets/images/front-closure.png',
+      video:
+        'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FFrontalClosure.mp4?alt=media&token=f5148a91-350e-48c4-ab3c-7e73a4d28d52',
       videos: [
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FFrontalClosure.mp4?alt=media&token=f5148a91-350e-48c4-ab3c-7e73a4d28d52',
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FFrontalClosure1.mp4?alt=media&token=0a7f6f63-6c1c-4206-93e7-9337389eec13'
@@ -62,6 +66,8 @@ export class HomePageComponent {
       price: 'From $119',
       type: 'Swiss Lace',
       image: 'assets/images/log_wave.jpg',
+      video:
+        'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FLong%20wavy%20hair%20bundles1.MP4?alt=media&token=523eca56-1b28-4bf7-900e-95577e713584',
       videos: [
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FLong%20wavy%20hair%20bundles.MP4?alt=media&token=773bd204-a4ea-4020-8398-3496c7779878',
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FLong%20wavy%20hair%20bundles1.MP4?alt=media&token=523eca56-1b28-4bf7-900e-95577e713584'
@@ -74,6 +80,8 @@ export class HomePageComponent {
       price: 'From $119',
       type: 'Swiss Lace',
       image: 'assets/images/indian-curly-hair-.jpg',
+      video:
+        'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FIndian%20Natural%20curly%20hair%20bundles2.mp4?alt=media&token=5035a0e2-0dd7-4a9e-b0ae-be44bd091974',
       videos: [
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FIndian%20Natural%20curly%20hair%20bundles.MOV?alt=media&token=3b3ccbeb-3a42-41f3-98b3-c6c311aa2605',
         'https://firebasestorage.googleapis.com/v0/b/the-haircraft.firebasestorage.app/o/hair-craft-videos%2FIndian%20Natural%20curly%20hair%20bundles2.mp4?alt=media&token=5035a0e2-0dd7-4a9e-b0ae-be44bd091974'
